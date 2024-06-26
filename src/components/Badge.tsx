a4f6d0e435f7c72fbb24c27bd300c63f9ec14290
kevthedev100 <136115811+kevthedev100@users.noreply.github.com>
2024-06-26 21:09:22 +0200

    Branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -48,17 +48,17 @@ export const Badge = (props: Props) => {
             'background-color': props.badgeBackgroundColor ?? '#ffffff',
           }}
         >
-          {props.footer?.text ?? 'Powered by'}
+          {props.footer?.text ?? 'Entwickelt vom'}
           <a
             ref={liteBadge}
-            href={props.footer?.companyLink ?? 'https://flowiseai.com'}
+            href={props.footer?.companyLink ?? 'https://ki-innovationszentrum.de/'}
             target="_blank"
             rel="noopener noreferrer"
             class="lite-badge"
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Flowise'}</span>
+            <span>&nbsp;{props.footer?.company ?? 'KI Innovationszentrum'}</span>
           </a>
         </span>
       </Show>
